1498c80eda1ca6f514b824e427c69dea9069cb4e
Pitou911 <songpitou723@gmail.com>
2023-04-12 16:06:31 -0500

    need the fix client component things

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import HomeHeader from "@/components/HomeHeader";
-
+import Image from "next/image";
+import HomeSearch from "@/components/HomeSearch";
 export default function Home() {
   return (
     <>
+      {/* Header */}
       <HomeHeader />
+
+      {/* Body */}
+      <div className='flex flex-col items-center mt-24'>
+        <Image
+          width='300'
+          height='100'
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png'
+          alt='logo'
+        />
+
+        <HomeSearch />
+      </div>
     </>
   );
 }
---
+++ src/components/HomeSearch.jsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsFillMicFill } from "react-icons/bs";
+
+export default function HomeSearch() {
+  function handleSubmit(e) {
+    e.preventdefault();
+  }
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className='flex w-full mt-5 mx-auto max-w-[90%] border 
+          border-gray-200 px-5 py-3 
+          rounded-full hover:shadow-md 
+          focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl'
+      >
+        <AiOutlineSearch className='text-xl text-gray-500 mr-3' />
+        <input type='text' className='flex-grow focus:outline-none' />
+        <BsFillMicFill className='text-lg' />
+      </form>
+
+      <div className='flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center sm:flex-row mt-8'>
+        <button className='btn'>Google Search</button>
+        <button className='btn'>I Am Feeling Lucky</button>
+      </div>
+    </>
+  );
+}
