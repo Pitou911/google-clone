38a5353c31ff9921576f5e7c854e508f7007071f
Pitou911 <songpitou723@gmail.com>
2023-04-15 14:10:38 -0500

    add body section

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -1,29 +1,67 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BsFillMicFill } from "react-icons/bs";
 
 export default function HomeSearch() {
-  function handleSubmit() {
-    console.log("hehe");
+  const router = useRouter();
+  const [input, setInput] = useState("");
+  const [randomSearchLoading, setRandomSearchLoading] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!input.trim()) return;
+    router.push(`/search/web?searchTerm=${input}`);
+  }
+
+  async function randomSearch() {
+    setRandomSearchLoading(true);
+    const response = await fetch("https://random-word-api.herokuapp.com/word")
+      .then((res) => res.json())
+      .then((data) => data[0]);
+    if (!response) return;
+    router.push(`/search/web?searchTerm=${response}`);
+    setRandomSearchLoading(false);
   }
   return (
     <>
       <form
-        onClick={handleSubmit}
+        onSubmit={handleSubmit}
         className='flex w-full mt-5 mx-auto max-w-[90%] border 
           border-gray-200 px-5 py-3 
           rounded-full hover:shadow-md 
           focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl'
       >
         <AiOutlineSearch className='text-xl text-gray-500 mr-3' />
-        <input type='text' className='flex-grow focus:outline-none' />
+        <input
+          type='text'
+          className='flex-grow focus:outline-none'
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
         <BsFillMicFill className='text-lg' />
       </form>
 
       <div className='flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center sm:flex-row mt-8'>
-        <button className='btn'>Google Search</button>
-        <button className='btn'>I Am Feeling Lucky</button>
+        <button onClick={handleSubmit} className='btn'>
+          Google Search
+        </button>
+        <button
+          disabled={randomSearchLoading}
+          onClick={randomSearch}
+          className='btn flex items-center justify-center disabled:op-80'
+        >
+          {randomSearchLoading ? (
+            <img
+              src='loading.svg'
+              alt='loading...'
+              className='h-6 text-center'
+            />
+          ) : (
+            "I Am Feeling Lucky"
+          )}
+        </button>
       </div>
     </>
   );
