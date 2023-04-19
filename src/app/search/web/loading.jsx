12f326a132b6c2134c6a51efd0903e6017fa35b1
Pitou911 <songpitou723@gmail.com>
2023-04-19 16:12:16 -0500

    add skeleton loading page

---
+++ src/app/search/web/loading.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function loading() {
+  return (
+    <>
+      <div className='mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse'>
+        <div className='bg-gray-200 rounded-full mb-2.5 h-2.5 w-48'></div>
+        <div className='bg-gray-200 rounded-full mb-2.5 h-3.5 w-[360px]'></div>
+        <div className='bg-gray-200 rounded-full mb-2.5 w-[560px] h-2'></div>
+        <div className='bg-gray-200 rounded-full mb-2.5 w-[530px] h-2'></div>
+      </div>
+      <div className='mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse'>
+        <div className='bg-gray-200 rounded-full mb-2.5 h-2.5 w-48'></div>
+        <div className='bg-gray-200 rounded-full mb-2.5 h-3.5 w-[360px]'></div>
+        <div className='bg-gray-200 rounded-full mb-2.5 w-[560px] h-2'></div>
+        <div className='bg-gray-200 rounded-full mb-2.5 w-[530px] h-2'></div>
+      </div>
+      <div className='mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse'>
+        <div className='bg-gray-200 rounded-full mb-2.5 h-2.5 w-48'></div>
+        <div className='bg-gray-200 rounded-full mb-2.5 h-3.5 w-[360px]'></div>
+        <div className='bg-gray-200 rounded-full mb-2.5 w-[560px] h-2'></div>
+        <div className='bg-gray-200 rounded-full mb-2.5 w-[530px] h-2'></div>
+      </div>
+      <div className='mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse'>
+        <div className='bg-gray-200 rounded-full mb-2.5 h-2.5 w-48'></div>
+        <div className='bg-gray-200 rounded-full mb-2.5 h-3.5 w-[360px]'></div>
+        <div className='bg-gray-200 rounded-full mb-2.5 w-[560px] h-2'></div>
+        <div className='bg-gray-200 rounded-full mb-2.5 w-[530px] h-2'></div>
+      </div>
+    </>
+  );
+}
