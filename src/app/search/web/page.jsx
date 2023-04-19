179d9c3b8ea7fed77ea4fb906a22e02a08e9ca08
Pitou911 <songpitou723@gmail.com>
2023-04-19 16:25:03 -0500

    fix timeout

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import React from "react";
 
 export default async function WebSearchPage({ searchParams }) {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  await new Promise((resolve) => setTimeout(resolve, 500));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
